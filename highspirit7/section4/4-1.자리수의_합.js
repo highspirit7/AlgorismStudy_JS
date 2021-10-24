f367e0211231990b62358f565a213aab90d988e2
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-10-25 00:02:24 +0900

    [highspirit7] section4-1 풀이 완료

--- highspirit7/section4/4-1.자리수의_합.js
+++ highspirit7/section4/4-1.자리수의_합.js
@@ -1,7 +1,27 @@
-function solution(n, arr) {
+function sumOfEachNumber(number) {
+	let string = number.toString();
+	let sum = 0;
 
-	return answer;
+	for (let i = 0; i < string.length; i++) {
+		sum += parseInt(string[i]);
+	}
+	return sum;
 }
 
-let arr = [128, 460, 603, 40, 521, 137, 123];
-console.log(solution(7, arr));
+function solution(n, arr) {
+	let maxNumIndex = 0;
+	let maxSum = 0;
+
+	for (let i = 0; i < n; i++) {
+		if (sumOfEachNumber(arr[i]) >= maxSum) {
+			maxSum = sumOfEachNumber(arr[i]);
+			maxNumIndex = i;
+			console.log(`index : ${i}`);
+			console.log(sumOfEachNumber(arr[i]));
+		}
+	}
+	return arr[maxNumIndex];
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123, 845, 111];
+console.log(solution(9, arr));
