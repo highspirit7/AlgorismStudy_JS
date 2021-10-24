193c766ca5ac5a5ceec7f2a036949dc78d5d1b47
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-10-24 22:55:17 +0900

    [highspirt7] section3-3 빠진 괄호 추가

--- highspirit7/section3/3-3.숫자만_추출.js
+++ highspirit7/section3/3-3.숫자만_추출.js
@@ -11,7 +11,7 @@ function solution(str) {
   // Number 메소드 같은 것 사용할 수 있지만 아래 로직으로
   // 숫자 앞에 0이 반복되는 경우를 제거해주었다.
   // 숫자 형태로 콘솔 출력하면 되는 문제라 굳이 숫자형태로 타입을 바꾸진 않았다.
-	if (answer[0] === '0') 
+	if (answer[0] === '0') {
 		let startingIndexToSlice = 1;
 		for (let i = 1; i < answer.length; i++) {
 			if (answer[i] === answer[i - 1]) {
