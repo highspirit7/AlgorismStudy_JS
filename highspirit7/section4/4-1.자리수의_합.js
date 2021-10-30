500bfca2625c24f39f2966b35f6d06d634c52be8
Jake Lee <jake.lee4006@gmail.com>
2021-10-31 00:42:56 +0900

    [highspirit7] section4-1 문제 로직 추가

--- highspirit7/section4/4-1.자리수의_합.js
+++ highspirit7/section4/4-1.자리수의_합.js
@@ -1,27 +1,31 @@
 function sumOfEachNumber(number) {
-	let string = number.toString();
-	let sum = 0;
+  let string = number.toString();
+  let sum = 0;
 
-	for (let i = 0; i < string.length; i++) {
-		sum += parseInt(string[i]);
-	}
-	return sum;
+  for (let i = 0; i < string.length; i++) {
+    sum += parseInt(string[i]);
+  }
+  return sum;
 }
 
 function solution(n, arr) {
-	let maxNumIndex = 0;
-	let maxSum = 0;
+  let maxNumIndex = 0;
+  let maxSum = 0;
 
-	for (let i = 0; i < n; i++) {
-		if (sumOfEachNumber(arr[i]) >= maxSum) {
-			maxSum = sumOfEachNumber(arr[i]);
-			maxNumIndex = i;
-			console.log(`index : ${i}`);
-			console.log(sumOfEachNumber(arr[i]));
-		}
-	}
-	return arr[maxNumIndex];
+  for (let i = 0; i < n; i++) {
+    if (sumOfEachNumber(arr[i]) >= maxSum) {
+      if (sumOfEachNumber(arr[i]) === maxSum) {
+        // 자릿수 합이 같은 경우 더 큰 숫자를 답으로 출력하기 위함.
+        if (arr[i] < arr[maxNumIndex]) continue;
+      }
+      maxSum = sumOfEachNumber(arr[i]);
+      maxNumIndex = i;
+      console.log(`index : ${i}`);
+      console.log(sumOfEachNumber(arr[i]));
+    }
+  }
+  return arr[maxNumIndex];
 }
 
-let arr = [128, 460, 603, 40, 521, 137, 123, 845, 111];
-console.log(solution(9, arr));
+let arr = [137, 460, 603, 40, 521, 128, 123, 111];
+console.log(solution(8, arr));
