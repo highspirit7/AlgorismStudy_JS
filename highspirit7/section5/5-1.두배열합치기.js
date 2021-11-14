f3ca6e17c3d52a5aac7ee8a4695ce0e7131dcb08
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-11-14 16:40:04 +0900

    [highspirit7] section5-1 풀이 코드 강의 참고하여 일부 수정

--- highspirit7/section5/5-1.두배열합치기.js
+++ highspirit7/section5/5-1.두배열합치기.js
@@ -5,20 +5,17 @@ function solution(arr1, arr2) {
 
   do {
     if (arr1[firstTarget] > arr2[secondTarget]) {
-      answer.push(arr2[secondTarget]);
-      secondTarget += 1;
+      // 아래와 같이 연산자를 뒤에 사용하면 먼저 기존 값을 사용한 다음에 1증가시킨다.
+      answer.push(arr2[secondTarget++]);
     } else if (arr1[firstTarget] < arr2[secondTarget]) {
-      answer.push(arr1[firstTarget]);
-      firstTarget += 1;
+      answer.push(arr1[firstTarget++]);
     } else {
       // 비교하는 두 값이 같은 경우 or 한 쪽이 undefined인 경우
       if (arr2[secondTarget]) {
-        answer.push(arr2[secondTarget]);
-        secondTarget += 1;
+        answer.push(arr2[secondTarget++]);
       }
       if (arr1[firstTarget]) {
-        answer.push(arr1[firstTarget]);
-        firstTarget += 1;
+        answer.push(arr1[firstTarget++]);
       }
     }
   } while (firstTarget < arr1.length || secondTarget < arr2.length);
@@ -27,5 +24,5 @@ function solution(arr1, arr2) {
 }
 
 let a = [1, 3, 5];
-let b = [2, 3, 6];
+let b = [2, 3, 6, 7, 9, 11];
 console.log(solution(a, b));
