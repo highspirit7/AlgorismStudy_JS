2a66cd3eccea2ab3df4fd813f3259a59e7b35c0c
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-12-19 22:38:24 +0900

    [highspirit7] section 5-6, 5-7 문제 풀이 완료

--- highspirit7/section5/5-7.아나그램.js
+++ highspirit7/section5/5-7.아나그램.js
@@ -1,9 +1,40 @@
-function solution(str1, str2){
-    let answer="YES"; 
+function solution(str1, str2) {
+  let answer = "YES";
+  const str1Map = new Map();
+  const str2Map = new Map();
 
-    return answer;
+  for (let s of str1) {
+    if (str1Map.has(s)) {
+      str1Map.set(s, str1Map.get(s) + 1);
+    } else {
+      str1Map.set(s, 1);
+    }
+  }
+
+  for (let s of str2) {
+    if (str2Map.has(s)) {
+      str2Map.set(s, str2Map.get(s) + 1);
+    } else {
+      str2Map.set(s, 1);
+    }
+  }
+
+  if (str1Map.size !== str2Map.size) answer = "NO";
+
+  for (const [key, value] of str1Map) {
+    if (!str1Map.has(key)) {
+      answer = "NO";
+      break;
+    }
+    if (str2Map.get(key) !== value) {
+      answer = "NO";
+      break;
+    }
+  }
+
+  return answer;
 }
 
-let a="AbaAeCe";
-let b="baeeACA";
+let a = "AbaAeCe";
+let b = "baeeACA";
 console.log(solution(a, b));
