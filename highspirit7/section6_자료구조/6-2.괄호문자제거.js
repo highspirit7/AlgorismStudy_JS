ddc4ded605dbe7a88268e227a3ecbf530087347c
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-03 22:41:33 +0900

    [highspirit7] 6-2 문제 테스트(jest) 파일 제작

--- highspirit7/section6_자료구조/6-2.괄호문자제거.js
+++ highspirit7/section6_자료구조/6-2.괄호문자제거.js
@@ -1,4 +1,4 @@
-function solution(s) {
+export default function solution(s) {
   const answer = [];
 
   for (let str of s) {
@@ -10,8 +10,8 @@ function solution(s) {
         answer.pop();
       }
     }
-    console.log(answer);
-    console.log("--------");
+    // console.log(answer);
+    // console.log("--------");
   }
 
   return answer.join("");
@@ -20,5 +20,3 @@ function solution(s) {
 // 괄호가 양쪽으로 짝이 맞지 않게 들어오는 경우 "(" 혹은 ")" 괄호가 더 많이 들어오는 경우도
 // 핸들링하게 하려 했으나 복잡해져서 포기.
 // 문제에서 주어진것처럼 양쪽으로 짝이 맞게 괄호가 들어가는 경우만 핸들링하도록 하였다.
-let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)Z(DB)";
-console.log(solution(str));
