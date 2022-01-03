63f04fdc87e674741757c585a7757ceb94a4c230
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-03 21:42:17 +0900

    [highspirit7] section6-2번 문제 풀이 완료

--- highspirit7/section6_자료구조/6-2.괄호문자제거.js
+++ highspirit7/section6_자료구조/6-2.괄호문자제거.js
@@ -1,8 +1,24 @@
 function solution(s) {
-  let answer;
+  const answer = [];
 
-  return answer;
+  for (let str of s) {
+    if (str !== ")") answer.push(str);
+    else {
+      const lastIndexOfOpenBracket = answer.lastIndexOf("(");
+
+      while (lastIndexOfOpenBracket < answer.length) {
+        answer.pop();
+      }
+    }
+    console.log(answer);
+    console.log("--------");
+  }
+
+  return answer.join("");
 }
 
-let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+// 괄호가 양쪽으로 짝이 맞지 않게 들어오는 경우 "(" 혹은 ")" 괄호가 더 많이 들어오는 경우도
+// 핸들링하게 하려 했으나 복잡해져서 포기.
+// 문제에서 주어진것처럼 양쪽으로 짝이 맞게 괄호가 들어가는 경우만 핸들링하도록 하였다.
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)Z(DB)";
 console.log(solution(str));
