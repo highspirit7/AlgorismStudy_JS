2819234edc0818c4ae04c03078989d3d10eb84ec
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-16 17:08:53 +0900

    [highspirit7] section6-4문제 풀이 및 테스트 완료

--- highspirit7/section6_자료구조/6-4.후위식_연산.js
+++ highspirit7/section6_자료구조/6-4.후위식_연산.js
@@ -1,8 +1,36 @@
-function solution(s) {
-  let answer;
+/**
+ * 처음에 감이 오지 않아 관련 블로그에서 살짝 힌트 참고
+ */
+export default function solution(s) {
+  const stack = [];
+  const operation = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+  };
 
-  return answer;
+  for (let i = 0; i < s.length; i++) {
+    const convertedToNum = Number(s[i]);
+    if (isNaN(convertedToNum)) {
+      // 숫자가 아니고 연산부호인 경우
+      const numberB = stack.pop();
+      const numberA = stack.pop();
+      const resultOfOperation = operation[s[i]](numberA, numberB);
+      // console.log(resultOfOperation);
+      stack.push(resultOfOperation);
+    } else {
+      // 숫자인 경우
+      stack.push(convertedToNum);
+    }
+  }
+
+  return stack[0];
 }
 
-let str = "352+*9-";
-console.log(solution(str));
+// 3을 넣고 [3]
+// 5를 넣고 [3, 5]
+// 2를 넣고 [3, 5, 2]
+// +를 만나면 필요한 개수만큼 위(뒤) 쪽에서 숫자를 빼내 계산하고 결과를 집어 넣는다. [3, 7]
+// *를 만나도 위와 마찬가지. [21]
+// 9를 만나면 [21, 9]
