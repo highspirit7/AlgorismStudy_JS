359eda513327feba770235bba3c33d3861d57098
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-19 00:38:01 +0900

    [highspirit7] section6-7 풀이 완료

--- highspirit7/section6_자료구조/6-7.교육과정설계.js
+++ highspirit7/section6_자료구조/6-7.교육과정설계.js
@@ -1,9 +1,17 @@
 function solution(need, plan) {
   let answer = "YES";
+  const queue = [];
 
+  for (let className of plan) {
+    if (need.includes(className)) {
+      queue.push(className);
+    }
+  }
+
+  if (queue.join("") !== need) answer = "NO";
   return answer;
 }
 
-let a = "CBA";
-let b = "CBDAGE";
+let a = "CBAE";
+let b = "CDBAGE";
 console.log(solution(a, b));
