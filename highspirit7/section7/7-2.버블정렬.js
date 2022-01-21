db67b9e1ba91fd73512574eb12ea48a0781c5fc2
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-22 00:02:41 +0900

    [highspirit7] section7-2풀이 - 스왑 코드 수정

--- highspirit7/section7/7-2.버블정렬.js
+++ highspirit7/section7/7-2.버블정렬.js
@@ -9,9 +9,11 @@ function solution(arr) {
       console.log(`${i + 1} : ${arr[i + 1]}`);
 
       if (arr[i] > arr[i + 1]) {
-        const temp = arr[i + 1];
-        arr[i + 1] = arr[i];
-        arr[i] = temp;
+        // const temp = arr[i + 1];
+        // arr[i + 1] = arr[i];
+        // arr[i] = temp;
+
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
       }
       console.log(arr);
     }
@@ -24,6 +26,5 @@ function solution(arr) {
   return answer;
 }
 
-// let arr = [13, 5, 11, 7, 23, 15];
 let arr = [13, 5, 11, 7, 23, 15, 34, 22];
 solution(arr);
