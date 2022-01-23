9f36e3432bb24fd896e4548e2e92720c96014319
heedaelee <adguy72@gmail.com>
2022-01-23 16:09:00 +0900

    [heedaelee]4.졸업 수정

--- heedaelee/section4/4.졸업.js
+++ heedaelee/section4/4.졸업.js
@@ -26,8 +26,10 @@
 
 */
 
-function solution(num, tMoney, p) {
-  for (let j = 0; j < num; j++) {
+function solution(numOfStud, tMoney, p) {
+  let num = numOfStud;
+  for (let j = 0; j < numOfStud; j++) {
+    console.log(`j : ${j}, num : ${num}, numOfStud : ${numOfStud}`);
     let maxPrice = 0;
     let maxOnlyPriceIndex = 0;
     let maxOnlyPrice = 0;
@@ -36,6 +38,7 @@ function solution(num, tMoney, p) {
 
     //할인 되서 가장 높은 가격 index 알아내는 for문
     for (let i = 0; i < num; i++) {
+      console.log(`i : ${i}, num : ${num}`);
       let discountedPrice = p[i][0] * 0.5 + p[i][1];
       console.log("discountedPrice", discountedPrice);
       if (discountedPrice > maxPrice) {
@@ -49,7 +52,7 @@ function solution(num, tMoney, p) {
 
       if (maxOnlyPrice < p[i][0]) {
         maxOnlyPriceIndex = i;
-        maxOnlyPrice = p[i][0]
+        maxOnlyPrice = p[i][0];
         console.log("maxOnlyPriceIndex", maxOnlyPriceIndex);
         console.log("maxOnlyPrice", maxOnlyPrice);
       }
@@ -59,7 +62,7 @@ function solution(num, tMoney, p) {
     for (let i = 0; i < num; i++) {
       //가장 비싼 가격 상품의 학생일때, 할인 받는다
       if (i === maxOnlyPriceIndex) {
-        console.log('몇번도나?')
+        console.log("몇번도나?");
         totalPrice += p[i][0] * 0.5 + p[i][1];
         // console.log(`fsrtotal : ${totalPrice}`)
         //가장 비싼거 아니면, 모두 정상가격 받는다
@@ -69,6 +72,7 @@ function solution(num, tMoney, p) {
       }
     }
     console.log("총 선물 가격 : ", totalPrice);
+    console.log(`tMoney : ${tMoney}, totalPrice : ${totalPrice}`);
 
     if (tMoney < totalPrice) {
       //쌤 가진 돈 보다 더 비쌀때\
@@ -76,26 +80,28 @@ function solution(num, tMoney, p) {
       p.splice(maxPriceIndex, 1);
       //2차원 배열에서 1차원 배열 중 가장 비싼 가격의 배열 요소를 삭제 해준다. splice() 사용
       // 한 요소를 삭제했으니 총 인원도 1명 줄여야 반복 for문에서 에러가 안나옴.
+      console.log("num1 : ", num);
       num--;
       // p.forEach((element) => {
       //   console.log(element);
       // });
     } else {
       //쌤 가진 돈이 더 많을때
+      console.log("num2 : ", num);
       return num;
     }
   }
 }
 
 const stuNum = 6,
-  teacherMoney = 28;
+  teacherMoney = 44;
 const priceAndDelivery = [
-  [6, 6],
-  [2, 2],
-  [4, 3],
-  [4, 5],
-  [1, 4],
-  [4, 5],
+  [14, 16],
+  [12, 2],
+  [14, 3],
+  [8, 15],
+  [10, 10],
+  [14, 5],
 ];
 
-console.log('총 명수 : ', solution(stuNum, teacherMoney, priceAndDelivery));
+console.log("총 명수 : ", solution(stuNum, teacherMoney, priceAndDelivery));
