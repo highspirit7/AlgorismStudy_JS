daf099878fc19ffa714884b5a3ae593844ba3893
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-01-25 00:44:37 +0900

    [highspirit7] 7-4 삽입정렬 풀이 완료

--- highspirit7/section7/7-4.삽입정렬.js
+++ highspirit7/section7/7-4.삽입정렬.js
@@ -1,8 +1,26 @@
 function solution(arr) {
   let answer = arr;
 
+  for (let i = 1; i < arr.length; i++) {
+    const temp = arr[i];
+
+    for (let j = i - 1; j >= 0; j--) {
+      console.log(`temp : ${temp}`);
+      if (arr[j] > temp) {
+        arr[j + 1] = arr[j];
+
+        if (j === 0) arr[j] = temp;
+      } else {
+        arr[j + 1] = temp;
+
+        break;
+      }
+    }
+    // console.log(answer);
+  }
+
   return answer;
 }
 
-let arr = [11, 7, 5, 6, 10, 9];
+let arr = [4, 11, 7, 5, 6, 10, 9, 99, 23];
 console.log(solution(arr));
