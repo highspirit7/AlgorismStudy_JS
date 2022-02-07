9336f36e4503b76ce55a79be8a8bd1b1c9e9fda3
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-07 15:33:06 +0900

    [highspirit7] section7-5번 문제 풀이 완료

--- highspirit7/section7/7-5.LRU(삽입정렬응용).js
+++ highspirit7/section7/7-5.LRU(삽입정렬응용).js
@@ -1,4 +1,35 @@
-function solution(size, arr) {}
+function solution(size, arr) {
+	// size 크기만큼 0을 집어넣어서 캐시용 배열 생성
+	const cache = [...Array(size)].map((elem) => 0);
 
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+	for (let work of arr) {
+		const indexOfWorkInCache = cache.indexOf(work);
+		let position;
+		// case 1 : 캐시 안에 작업이 존재하는 경우
+		if (indexOfWorkInCache > -1) {
+			cache[indexOfWorkInCache] = 0;
+			position = indexOfWorkInCache;
+		} else {
+			if (cache[size - 1] > 0) {
+				// case 2 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 꽉 차 있을 때, 마지막 인덱스에 위치한 작업을 삭제하고
+				cache[size - 1] = 0;
+			}
+
+			// case 3 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 주어진 사이즈보다 작을 때
+			position = size - 1;
+		}
+
+		while (position >= 0) {
+			if (position === 0) cache[position] = work;
+			else cache[position] = cache[position - 1];
+
+			position--;
+		}
+		console.log(cache);
+	}
+
+	return cache;
+}
+
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7, 4];
 console.log(solution(5, arr));
