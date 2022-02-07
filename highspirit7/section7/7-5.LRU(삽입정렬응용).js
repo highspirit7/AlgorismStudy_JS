62ea42bfdfa3331fc0d3d280d8d06d7ddbcf67a4
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-07 17:55:54 +0900

    [highspirit7] section 7-5번 문제 리팩토링 및 주석 수정

--- highspirit7/section7/7-5.LRU(삽입정렬응용).js
+++ highspirit7/section7/7-5.LRU(삽입정렬응용).js
@@ -1,3 +1,9 @@
+/**
+ * @history
+ * 7th Feb 2022(ver0) : Cache miss일 경우, 무조건 배열 제일 뒤에서부터 순회하도록 처리
+ * 7th Feb 2022(ver1) : 앞 뒤 모두 0인 경우 시프트하는게 의미가 없기 때문에 제일 처음 0인 요소의 인덱스부터 순회하도록 수정
+ */
+
 function solution(size, arr) {
 	// size 크기만큼 0을 집어넣어서 캐시용 배열 생성
 	const cache = [...Array(size)].map((elem) => 0);
@@ -5,18 +11,16 @@ function solution(size, arr) {
 	for (let work of arr) {
 		const indexOfWorkInCache = cache.indexOf(work);
 		let position;
-		// case 1 : 캐시 안에 작업이 존재하는 경우
+		// case 1 : 캐시 안에 작업이 존재하는 경우 ; Cache Hit
 		if (indexOfWorkInCache > -1) {
 			cache[indexOfWorkInCache] = 0;
 			position = indexOfWorkInCache;
 		} else {
-			if (cache[size - 1] > 0) {
-				// case 2 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 꽉 차 있을 때, 마지막 인덱스에 위치한 작업을 삭제하고
-				cache[size - 1] = 0;
-			}
-
-			// case 3 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 주어진 사이즈보다 작을 때
-			position = size - 1;
+			// 아래 2가지 케이스 모두 Cache Miss
+			// case 2 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 꽉 차 있을 때, 배열의 가장 뒤에서부터 앞으로 순회하며 시프트.
+			// case 3 : 캐시 안에 작업이 없고 캐시 작업 사이즈가 주어진 사이즈보다 작을 때,
+			// 제일 처음으로 0이 담겨 있는 그러니까 비어있는 인덱스부터 앞으로 순회하며 시프트.
+			position = cache.indexOf(0) === -1 ? size - 1 : cache.indexOf(0);
 		}
 
 		while (position >= 0) {
@@ -31,5 +35,5 @@ function solution(size, arr) {
 	return cache;
 }
 
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7, 4];
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
 console.log(solution(5, arr));
