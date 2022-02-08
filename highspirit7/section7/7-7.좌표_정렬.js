b347dd69ab42eaa2e1166bc7075626cada0ed685
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-08 13:00:42 +0900

    [highspirit7] section7-7 풀이 완료

--- highspirit7/section7/7-7.좌표_정렬.js
+++ highspirit7/section7/7-7.좌표_정렬.js
@@ -1,14 +1,24 @@
 function solution(arr) {
-  let answer = arr;
+	let answer = arr;
 
-  return answer;
+	answer.sort((coordinateA, coordinateB) => {
+		if (coordinateA[0] === coordinateB[0]) {
+			return coordinateA[1] - coordinateB[1];
+		} else {
+			return coordinateA[0] - coordinateB[0];
+		}
+	});
+
+	return answer;
 }
 
 let arr = [
-  [2, 7],
-  [1, 3],
-  [1, 2],
-  [2, 5],
-  [3, 6],
+	[2, 7],
+	[1, 3],
+	[1, 2],
+	[2, 5],
+	[3, 6],
+	[2, 4],
+  [1, 3]
 ];
 console.log(solution(arr));
