89bb17f196f97d43a78936ddf4af0276b9dcbada
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-08 13:13:27 +0900

    [highspirit7] section7-7 로직 수정(ver1)

--- highspirit7/section7/7-7.좌표_정렬.js
+++ highspirit7/section7/7-7.좌표_정렬.js
@@ -1,12 +1,14 @@
+/**
+ * @history
+ * 8th Feb 2022(ver0) : if, else 사용
+ * 8th Feb 2022(ver1) : 삼항연산자로 변경
+ */
+
 function solution(arr) {
 	let answer = arr;
 
 	answer.sort((coordinateA, coordinateB) => {
-		if (coordinateA[0] === coordinateB[0]) {
-			return coordinateA[1] - coordinateB[1];
-		} else {
-			return coordinateA[0] - coordinateB[0];
-		}
+		return coordinateA[0] === coordinateB[0] ? coordinateA[1] - coordinateB[1] : coordinateA[0] - coordinateB[0];
 	});
 
 	return answer;
@@ -19,6 +21,6 @@ let arr = [
 	[2, 5],
 	[3, 6],
 	[2, 4],
-  [1, 3]
+	[1, 3],
 ];
 console.log(solution(arr));
