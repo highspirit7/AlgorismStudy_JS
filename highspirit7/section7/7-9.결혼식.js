4e6a7a7b84a6f86352f65b755f76c0e11d718d7a
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-10 21:08:45 +0900

    [highspirit7] section7-9 풀이 완료

--- highspirit7/section7/7-9.결혼식.js
+++ highspirit7/section7/7-9.결혼식.js
@@ -1,14 +1,32 @@
 function solution(times) {
-  let answer = Number.MIN_SAFE_INTEGER;
+	let answer = Number.MIN_SAFE_INTEGER;
 
-  return answer;
+	times.sort((comingTime, leavingTime) => {
+		return comingTime[0] === leavingTime[0] ? comingTime[1] - leavingTime[1] : comingTime[0] - leavingTime[0];
+	});
+	console.log(times);
+	for (let i = 0; i < times.length; i++) {
+		let maxNumOfPeople = 1;
+		for (let j = 0; j < times.length; j++) {
+			if (i === j) continue;
+			// 친구의 오는 시간(times[j])이 기준이 되는 친구(times[i])의 가는 시간보다는 작아야 하고
+			// 오는 시간보다는 크거나 같아야 한다.
+			if (times[j][0] < times[i][1] && times[j][0] >= times[i][0]) maxNumOfPeople++;
+		}
+		console.log(`i : ${i}`);
+		console.log(`maxNumOfPeople : ${maxNumOfPeople}`);
+		if (maxNumOfPeople > answer) answer = maxNumOfPeople;
+	}
+
+	return answer;
 }
 
 let arr = [
-  [14, 18],
-  [12, 15],
-  [15, 20],
-  [20, 30],
-  [5, 14],
+	[14, 18],
+	[12, 15],
+	[15, 20],
+	[20, 30],
+	[5, 14],
+	[7, 16],
 ];
 console.log(solution(arr));
