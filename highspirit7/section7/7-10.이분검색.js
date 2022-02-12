0d02b492d7029e9e6a4d5685ab9196ec55234db1
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-12 19:09:26 +0900

    [highspirit7] section7-10 풀이완료(ver0)

--- highspirit7/section7/7-10.이분검색.js
+++ highspirit7/section7/7-10.이분검색.js
@@ -1,8 +1,44 @@
-function solution(target, arr) {
-  let answer;
+/**
+ * @history
+ * 12th Feb 2022(ver0)
+ */
 
-  return answer;
+function solution(target, arr) {
+	let answer;
+
+	arr.sort((a, b) => a - b);
+	console.log(arr);
+
+	let start = 0;
+	let end = arr.length - 1;
+	let mid = Math.floor((start + end) / 2);
+	console.log(`start : ${start}`);
+	console.log(`mid : ${mid}`);
+	console.log(`end : ${end}`);
+
+	while (!answer) {
+		if (arr[mid] < target) {
+			start = mid;
+
+			if (end === arr.length - 1 && end - mid === 1) {
+				mid = Math.ceil((start + end) / 2);
+			} else {
+				mid = Math.floor((start + end) / 2);
+			}
+		} else if (arr[mid] > target) {
+			end = mid;
+			mid = Math.floor((start + end) / 2);
+		} else if (arr[mid] === target) {
+			answer = mid;
+			break;
+		}
+		console.log(`start : ${start}`);
+		console.log(`mid : ${mid}`);
+		console.log(`end : ${end}`);
+	}
+
+	return answer + 1;
 }
 
-let arr = [23, 87, 65, 12, 57, 32, 99, 81];
-console.log(solution(32, arr));
+let arr = [23, 87, 65, 12, 57, 32, 99, 81, 77];
+console.log(solution(12, arr));
