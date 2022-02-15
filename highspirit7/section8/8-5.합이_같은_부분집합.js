fdd245e5f1a28d1432d47651a99fbe79b1755cb6
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-15 22:03:25 +0900

    [highspirit7] section8 문제 셋업

---
+++ highspirit7/section8/8-5.합이_같은_부분집합.js
@@ -0,0 +1,6 @@
+function solution(arr) {
+
+}
+
+let arr = [1, 3, 5, 6, 7, 10];
+console.log(solution(arr));
---
+++ highspirit7/section8/8-6.바둑이_승차.js
@@ -0,0 +1,6 @@
+function solution(c, arr) {
+
+}
+
+let arr = [81, 58, 42, 33, 61];
+console.log(solution(259, arr));
