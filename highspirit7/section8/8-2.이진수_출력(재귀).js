24360cdc17a4e03ae09fac557e090e5e83602d3d
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-17 16:48:18 +0900

    [highspirit7] section8-2풀이 완료

--- highspirit7/section8/8-2.이진수_출력(재귀).js
+++ highspirit7/section8/8-2.이진수_출력(재귀).js
@@ -1,7 +1,27 @@
 function solution(n) {
 	let answer = '';
 
+	function recursive(number) {
+		const quotient = Math.floor(number / 2);
+
+		if (quotient === 0) {
+			answer += '1';
+      console.log(`number : ${number}`);
+			console.log(`answer : ${answer}`);
+			return;
+		}
+
+		recursive(quotient);
+
+		if (number % 2 === 0 || quotient === 2) answer += '0';
+		else answer += '1';
+		console.log(`number : ${number}`);
+		console.log(`answer : ${answer}`);
+	}
+
+	recursive(n);
+
 	return answer;
 }
 
-console.log(solution(11));
+console.log(solution(555));
