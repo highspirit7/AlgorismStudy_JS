0b78ff4082389ffb1978f4ad2cd299d3c7d3cfbe
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-18 19:41:02 +0900

    [highspirit7] section 8-3 풀이 완료

--- highspirit7/section8/8-3.이진트리순회.js
+++ highspirit7/section8/8-3.이진트리순회.js
@@ -1,7 +1,39 @@
-function solution(n) {
-	let answer = '';
+// 탐색 우선 순위
+// 전위 순회(깊이 우선 탐색 ; DFS) : 부모 > 왼쪽 자식 > 오른쪽 자식
+// 중위 순회 : 왼쪽 자식 > 부모 > 오른쪽 자식
+// 후위 순회 : 왼쪽 자식 > 오른쪽 자식 > 부모
 
-	return answer;
+function solution(root) {
+	function preorderTraversal(node) {
+		if (node > 7) return;
+		else {
+			console.log(node);
+			preorder(node * 2);
+			preorder(node * 2 + 1);
+		}
+	}
+
+	function inorderTraversal(node) {
+		if (node > 7) return;
+		else {
+			inorderTraversal(node * 2);
+			console.log(node);
+			inorderTraversal(node * 2 + 1);
+		}
+	}
+
+	function postorderTraversal(node) {
+		if (node > 7) return;
+		else {
+			postorderTraversal(node * 2);
+			postorderTraversal(node * 2 + 1);
+			console.log(node);
+		}
+	}
+
+	// preorderTraversal(root);
+	// inorderTraversal(root);
+	postorderTraversal(root);
 }
 
 console.log(solution(1));
