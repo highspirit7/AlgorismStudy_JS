5b0a169a5ccc94b9f7dd2e40a856cf4b0657b90a
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-21 10:31:24 +0900

    [highspirit7] section 8-9 풀이 완료(ver0)

--- highspirit7/section8/8-9.동전교환.js
+++ highspirit7/section8/8-9.동전교환.js
@@ -1,5 +1,27 @@
-function solution(m, arr) {
+/**
+ * 동전교환
+ * @history
+ * 21th Feb 2022(ver0)
+ */
 
+function solution(m, arr) {
+	let minCount = Number.MAX_SAFE_INTEGER;
+	let count = 0;
+
+	function DFS(count, sum) {
+		if (sum > m) return;
+		else if (sum === m) {
+			minCount = Math.min(count, minCount);
+		} else {
+			for (let i = 0; i < arr.length; i++) {
+				DFS(count + 1, sum + arr[i]);
+			}
+		}
+	}
+
+	DFS(0, 0);
+
+	return minCount;
 }
 
 let arr = [1, 2, 5];
