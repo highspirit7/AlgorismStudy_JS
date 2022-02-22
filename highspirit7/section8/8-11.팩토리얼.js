63e4165bab00d3e1252d2e1c561bc49f3c035e63
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-22 11:42:31 +0900

    [highspirit7] section8-11 풀이 수정(ver1)

--- highspirit7/section8/8-11.팩토리얼.js
+++ highspirit7/section8/8-11.팩토리얼.js
@@ -2,23 +2,23 @@
  * 팩토리얼
  * @history
  * 22th Feb 2022(ver0)
+ * 22th Feb 2022(ver1) : 재귀함수 값을 return하여 변수에 저장하는 방식을 
+ * 강의 정답에서 배우게 되었고 해당 방식으로 코드 수정
  */
 
 function solution(n) {
-	let answer = 1;
+	let answer;
 
 	function factorial(number) {
-		if (number === 1) {
-			return;
-		} else {
-			answer = answer * number;
-			factorial(number - 1);
+		if (number === 1) return 1;
+		else {
+			return number * factorial(number - 1);
 		}
 	}
 
-	factorial(n);
+	answer = factorial(n);
 
 	return answer;
 }
 
-console.log(solution(7));
+console.log(solution(5));
