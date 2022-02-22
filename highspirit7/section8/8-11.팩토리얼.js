5023289bf6b33a1ebdb611216aea03a5c29ac916
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-22 10:49:57 +0900

    [highspirit7] section 8-11 풀이 완료(ver0)

--- highspirit7/section8/8-11.팩토리얼.js
+++ highspirit7/section8/8-11.팩토리얼.js
@@ -1,7 +1,24 @@
+/**
+ * 팩토리얼
+ * @history
+ * 22th Feb 2022(ver0)
+ */
+
 function solution(n) {
-	let answer;
+	let answer = 1;
+
+	function factorial(number) {
+		if (number === 1) {
+			return;
+		} else {
+			answer = answer * number;
+			factorial(number - 1);
+		}
+	}
+
+	factorial(n);
 
 	return answer;
 }
 
-console.log(solution(5));
+console.log(solution(7));
