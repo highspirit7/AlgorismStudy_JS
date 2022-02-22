8a97bf32eb0872b77832a385cd4c38c72db30731
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-22 23:12:13 +0900

    [highspirit7] section 8-12 풀이 완료

--- highspirit7/section8/8-12.조합수(메모이제이션).js
+++ highspirit7/section8/8-12.조합수(메모이제이션).js
@@ -1,6 +1,36 @@
-function solution(n, r) {
-	let answer = [];
+function factorial(number) {
+	if (number === 1) return 1;
+	else {
+		return number * factorial(number - 1);
+	}
+}
 
+function solution(n, r) {
+	let answer;
+	let memoization = Array.from(Array(r + 1), () => Array(n + 1).fill(0));
+
+	function DFS(total, taken) {
+		if (memoization[taken][total] > 0) {
+			return memoization[taken][total];
+		}
+		console.log(`n : ${taken}, r : ${total}`);
+		if (taken === 0) {
+			// 뽑아야할 숫자 개수가 0일 때 경우의 수는 1가지이다.
+			return 1;
+		} else if (taken === total) {
+			// 주어진 숫자 개수와 뽑아야할 숫자 개수가 같으면 경우의 수는 1가지이다.
+			return 1;
+		} else {
+			// 아래와 같이 리턴하면 어떻게 되는 지 몰랐었다. 등호 좌측이든 우측이든
+			// 연산된 결과값이 리턴된다!!!
+			return (memoization[taken][total] = DFS(total - 1, taken - 1) + DFS(total - 1, taken));
+		}
+	}
+
+	DFS(n, r);
+
+	answer = memoization[r][n];
+	console.log(memoization);
 	return answer;
 }
 
