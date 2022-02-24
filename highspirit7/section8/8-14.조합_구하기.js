ef9315a1c6651e722cc3054ff8031473f8272706
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-24 17:50:38 +0900

    [highspirit7] section 8-14 풀이 완료

--- highspirit7/section8/8-14.조합_구하기.js
+++ highspirit7/section8/8-14.조합_구하기.js
@@ -1,7 +1,26 @@
 function solution(n, m) {
+	const base = Array.from({ length: n }, (e, i) => i + 1);
 	let answer = [];
+	let temp = [];
 
-	return answer;
+	function DFS(level, starter) {
+		if (level === m) {
+			answer.push(temp.slice());
+			console.log(temp.slice());
+		} else {
+			for (let i = starter; i < n; i++) {
+				temp.push(base[i]);
+
+				DFS(level + 1, i + 1);
+
+				temp.pop();
+			}
+		}
+	}
+
+	DFS(0, 0);
+
+	return answer.length;
 }
 
 console.log(solution(4, 2));
