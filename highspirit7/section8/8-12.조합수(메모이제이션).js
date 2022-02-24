bbb2e2c82e8d7ca5d6d2bfb71184f3566dbb5712
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-02-24 12:19:29 +0900

    [highspirit7] section 8-12 로직 수정

--- highspirit7/section8/8-12.조합수(메모이제이션).js
+++ highspirit7/section8/8-12.조합수(메모이제이션).js
@@ -14,11 +14,12 @@ function solution(n, r) {
 			return memoization[taken][total];
 		}
 		console.log(`n : ${taken}, r : ${total}`);
-		if (taken === 0) {
+		if (taken === 0 || taken === total) {
 			// 뽑아야할 숫자 개수가 0일 때 경우의 수는 1가지이다.
-			return 1;
-		} else if (taken === total) {
 			// 주어진 숫자 개수와 뽑아야할 숫자 개수가 같으면 경우의 수는 1가지이다.
+
+      // n과 r이 같거나 r이 0인 경우에도 제대로 값(1)을 출력하기 위한 코드
+			memoization[taken][total] = 1;
 			return 1;
 		} else {
 			// 아래와 같이 리턴하면 어떻게 되는 지 몰랐었다. 등호 좌측이든 우측이든
@@ -34,4 +35,4 @@ function solution(n, r) {
 	return answer;
 }
 
-console.log(solution(5, 3));
+console.log(solution(4, 0));
