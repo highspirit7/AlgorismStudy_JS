63fa226258adf7d6ea9c0a6e183b3f888d28bf04
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-03-13 16:38:44 +0900

    [highspirit7] section5-1에 시간복잡도 정보 추가

--- highspirit7/section5_효율성/5-1.두배열합치기.js
+++ highspirit7/section5_효율성/5-1.두배열합치기.js
@@ -1,26 +1,28 @@
+// 시간 복잡도는 O(N)
+// https://blog.naver.com/ndb796/221227934987
 function solution(arr1, arr2) {
-  let answer = [];
-  let firstTarget = 0,
-    secondTarget = 0;
+	let answer = [];
+	let firstTarget = 0,
+		secondTarget = 0;
 
-  do {
-    if (arr1[firstTarget] > arr2[secondTarget]) {
-      // 아래와 같이 연산자를 뒤에 사용하면 먼저 기존 값을 사용한 다음에 1증가시킨다.
-      answer.push(arr2[secondTarget++]);
-    } else if (arr1[firstTarget] < arr2[secondTarget]) {
-      answer.push(arr1[firstTarget++]);
-    } else {
-      // 비교하는 두 값이 같은 경우 or 한 쪽이 undefined인 경우
-      if (arr2[secondTarget]) {
-        answer.push(arr2[secondTarget++]);
-      }
-      if (arr1[firstTarget]) {
-        answer.push(arr1[firstTarget++]);
-      }
-    }
-  } while (firstTarget < arr1.length || secondTarget < arr2.length);
-  // 두 배열 다 끝까지 순회할때까지 반복
-  return answer;
+	do {
+		if (arr1[firstTarget] > arr2[secondTarget]) {
+			// 아래와 같이 연산자를 뒤에 사용하면 먼저 기존 값을 사용한 다음에 1증가시킨다.
+			answer.push(arr2[secondTarget++]);
+		} else if (arr1[firstTarget] < arr2[secondTarget]) {
+			answer.push(arr1[firstTarget++]);
+		} else {
+			// 비교하는 두 값이 같은 경우 or 한 쪽이 undefined인 경우
+			if (arr2[secondTarget]) {
+				answer.push(arr2[secondTarget++]);
+			}
+			if (arr1[firstTarget]) {
+				answer.push(arr1[firstTarget++]);
+			}
+		}
+	} while (firstTarget < arr1.length || secondTarget < arr2.length);
+	// 두 배열 다 끝까지 순회할때까지 반복
+	return answer;
 }
 
 let a = [1, 3, 5];
