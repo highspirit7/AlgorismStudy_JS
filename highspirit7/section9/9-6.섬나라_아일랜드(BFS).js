d7ea4ce09d5bd4b51e923b1ba5c6515fbf6cc3c6
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-03-17 23:25:41 +0900

    [highspirit7] section9-6 풀이 완료(ver0)

--- highspirit7/section9/9-6.섬나라_아일랜드(BFS).js
+++ highspirit7/section9/9-6.섬나라_아일랜드(BFS).js
@@ -1,5 +1,39 @@
 function solution(board) {
 	let answer = 0;
+	let queue = [];
+
+	// sx : 격자판 세로 사이즈, sy : 격자판 가로 사이즈
+	const [sx, sy] = [board.length, board[0].length];
+	const dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+	const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+	queue.push([0, 0]);
+
+	for (let i = 0; i < board.length; i++) {
+		for (let j = 0; j < board[i].length; j++) {
+			if (board[i][j] === 1) {
+				answer++;
+				queue.push([i, j]);
+
+				// 섬 하나 찾을 때마다 그 시점의 격자판 지도 상태를 보기 위함.
+				// console.log(deepCopy(board));
+
+				while (queue.length > 0) {
+					let [cx, cy] = queue.shift();
+					board[cx][cy] = 0;
+					console.log(deepCopy(queue));
+					for (let i = 0; i < 8; i++) {
+						let nx = cx + dx[i];
+						let ny = cy + dy[i];
+
+						if (nx >= 0 && ny >= 0 && nx < sx && ny < sy && board[nx][ny] === 1) {
+							queue.push([nx, ny]);
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return answer;
 }
@@ -15,3 +49,22 @@ let arr = [
 ];
 
 console.log(solution(arr));
+
+function deepCopy(inObject) {
+	let outObject, value, key;
+
+	if (typeof inObject !== 'object' || inObject === null) {
+		return inObject;
+	}
+
+	outObject = Array.isArray(inObject) ? [] : {};
+
+	for (key in inObject) {
+		value = inObject[key];
+
+		// Recursively (deep) copy for nested objects, including arrays
+		outObject[key] = deepCopy(value);
+	}
+
+	return outObject;
+}
